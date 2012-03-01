38772f8b136037f77b79f8ad9a27cabfc038401e
Yves Dessertine <yves.dessertine2@gmail.com>
2012-03-01 02:11:24 +0100

    Version 20
     - modification des messages affichés
     - suppression permission internet (inutile depuis v19), et effacement des mots de passe stockés

--- src/net/yvesd/scfm/RipActivity.java
+++ src/net/yvesd/scfm/RipActivity.java
@@ -1,11 +1,15 @@
 package net.yvesd.scfm;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RipActivity extends Activity {
 
@@ -13,18 +17,62 @@ public class RipActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		supprimerPreferences();
+
 		setContentView(R.layout.main_layout);
 
-		Button market = (Button) findViewById(R.id.market);
-		addClickListener(market, "market://details?id=net.yvesd.scfm");
+		Button espaceAbonne = (Button) findViewById(R.id.espaceAbonne);
+		addClickListener(this, espaceAbonne,
+				"https://mobile.free.fr/moncompte/");
 
-		Button source = (Button) findViewById(R.id.source);
-		addClickListener(source,
-				"https://github.com/yvesd/SuiviConsoFreeMobile/");
+		Button officialApps = (Button) findViewById(R.id.officialApps);
+		addClickListener(this, officialApps, "market://search?q=pub:FreeMobile");
+
+		Button market = (Button) findViewById(R.id.market);
+		addClickListener(this, market, "market://details?id=net.yvesd.scfm");
+
+		Button quitter = (Button) findViewById(R.id.quitter);
+		quitter.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 
-	private void addClickListener(Button market, final String uri) {
-		market.setOnClickListener(new View.OnClickListener() {
+	/**
+	 * Delete the application preferences (login and password ). They are not
+	 * used anymore starting from v19
+	 */
+	private void supprimerPreferences() {
+
+		try {
+			SharedPreferences settings = getSharedPreferences(
+					"SuiviConsoFreeMobilePrefs", 0);
+			Editor ed = settings.edit();
+			ed.clear();
+			ed.commit();
+		} catch (Exception e) {
+			// Ne rien faire ici
+		}
+	}
+
+	/**
+	 * Adds an OnClickListener with an Intent of ACTION_VIEW, and the Uri
+	 * specified
+	 * 
+	 * @param context
+	 *            The application context. Used to display toast in case of
+	 *            failure
+	 * @param targetButton
+	 *            the button which will recieve the OnClickListener
+	 * @param uri
+	 *            The target Uri, which is displayed when the user clicks the
+	 *            button
+	 */
+	private void addClickListener(final Context context, Button targetButton,
+			final String uri) {
+		targetButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -32,7 +80,8 @@ public class RipActivity extends Activity {
 				try {
 					startActivity(i);
 				} catch (Exception e) {
-					// ...
+					Toast.makeText(context, R.string.action_non_dispo,
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
